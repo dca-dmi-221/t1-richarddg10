53a3ea52853bdb9036123c7ac107b9ac7e48cf45
richarddg10 <1003698056@u.icesi.edu.co>
2022-04-04 23:03:45 -0500

    "Avance 3"

--- BotonPlay.js
+++ BotonPlay.js
@@ -1,20 +1,9 @@
 class BotonPlay {
     constructor() {
-        this.listaCanciones = listaCanciones
     }
 
-    show() {
+    /*show() {
         createButton("play")
         //button.position(125, 350)
-    }
-
-    cambio() {
-        if (!this.listaCanciones.isPlaying()) {
-            this.listaCanciones.play()
-            button.html("pause")
-        } else {
-            this.listaCanciones.pause()
-            button.html("play")
-        }
-    }
+    }*/
 }
--- sketch.js
+++ sketch.js
@@ -3,35 +3,88 @@ let reproductor
 let input
 let listaCanciones
 
-let boton
+let btnPlayPause
+let btnStop
+let btnNext
+let btnPrevious
+
+let sVolume
+
+let music = []
+i = 0
 
 function setup() {
-  createCanvas(350, 600)
+  createCanvas(windowWidth, windowHeight)
   fondos = new Fondo(0, 0, 350, 600)
-  listaCanciones = []
-  input = createFileInput(this.manejarArchivo)
-  reproductor = new Reproductor(listaCanciones)
-  boton = new BotonPlay()
-  boton.show()
-  //boton.position(125, 350)
+  //listaCanciones = []
+  //input = createFileInput(this.manejarArchivo)
+  //reproductor = new Reproductor(listaCanciones)
+  //boton = new BotonPlay()
+  //boton.show()
+  btnPlayPause = createButton("Play")
+  btnPlayPause.position(125, 350)
+  btnPlayPause.mousePressed(playPause)
+
+  btnStop = createButton("Stop")
+  btnStop.position(125, 380)
+  btnStop.mousePressed(stop)
+
+  btnNext = createButton("Next")
+  btnNext.position(170, 350)
+
+  btnPrevious = createButton("Previous")
+  btnPrevious.position(50, 350)
+
+  sVolume = createSlider(0, 1, 0.5, 0.01)
+  sVolume.position(90, 400)
 }
 
 function draw() {
   fondos.show()
-
+  music[i].setVolume(sVolume.value())
 }
 
+function preload(){
+  music[0] = loadSound("pantysito.mp3")
+  music[1] = loadSound("la_inocente.mp3")
+}
+
+/*
 function manejarArchivo(file) {
   console.log(file.type)
 
   if (file.type === 'audio') {
     let musica = loadSound(file, () => {
       listaCanciones.push(musica)
-      //listaCanciones[0].play()
+      listaCanciones[0].play()
       //listaCanciones[0].currentTime()
-      boton.cambio()
       console.log(listaCanciones[0])
       console.log(getAudioContext().state)
     })
   }
+}
+
+
+function playPause() {
+  if (!this.listaCanciones[i].isPlaying()) {
+      this.listaCanciones[i].play()
+      btnPlayPause.html("Pause")
+  } else {
+      this.listaCanciones[i].pause()
+      btnPlayPause.html("Play")
+  }
+}*/
+
+function playPause() {
+  if (!music[i].isPlaying()) {
+      music[i].play()
+      btnPlayPause.html("Pause")
+  } else {
+      music[i].pause()
+      btnPlayPause.html("Play")
+  }
+}
+
+function stop(){
+  music[i].stop()
 }
